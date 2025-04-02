85c765658b71819e7c77285bfd00cac7ab398dc5
anirudh6415 <anirudh.9591@gmail.com>
2025-04-02 18:39:49 -0400

    first commit

--- main.py
+++ main.py
@@ -3,32 +3,32 @@
 Entry point of ColdContactXLSX.
 """
 
-import logging
+
 import email_utils.follow_up as follow_up
 
 from scheduler.schedule_now import send_emails_now
 from scheduler.send_later import schedule_emails
 
 # Set up logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from logger import get_logger
+logger = get_logger(__name__)
 
 def main():
-    choice = input("Is this the first email or a follow-up? (first/follow-up): ").lower()
+    choice = input("Is this the first email or a follow-up? (1 for first, 2 for follow-up): ").strip()
 
-    if choice == 'first':
+    if choice == '1':
         first_email_flow()
-    elif choice == 'follow-up':
+    elif choice == '2':
         follow_up_flow()
     else:
         print("Invalid choice. Please enter 'first' or 'follow-up'.")
 
 def first_email_flow():
-    choice = input("Do you want to send the email now or schedule it for a specific time? (now/schedule): ").lower()
+    choice = input("Do you want to send the email now or schedule it? (1 for now, 2 for schedule): ").strip()
 
-    if choice == 'now':
+    if choice == '1':
         send_emails_now()
-    elif choice == 'schedule':
+    elif choice == '2':
         schedule_emails()
     else:
         print("Invalid choice. Please enter 'now' or 'schedule'.")
